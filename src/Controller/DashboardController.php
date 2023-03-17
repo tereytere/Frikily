a9b89d90c9cde4d3a83b43e9d90ee009f5d67da9
Teresa <teresa.ambroa@hotmail.com>
2023-03-17 18:10:41 +0100

    paginationFail

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,18 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, PaginatorInterface $paginator, Request $request): Response
     {
         $em = $doctrine->getManager();
-        $posts = $em->getRepository(Posts::class)->findAll();
+        $query = $em->getRepository(Posts::class)->BuscarTodosLosPosts();
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
         return $this->render('dashboard/index.html.twig', [
-            'posts' => $posts,
+            'pagination' => $pagination,
         ]);
     }
 }
